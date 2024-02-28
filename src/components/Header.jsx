157cf16fae8016074d12a9db530dba58eede42e2
Emma <mellgrenemma0@gmail.com>
2024-02-28 17:41:28 +0100

    Update project title and add header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <div>
+        <nav>
+        </nav>
+        <ul>
+            <li>Contact Me</li>
+            <li></li>
+            <li>Contact Me</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Header
